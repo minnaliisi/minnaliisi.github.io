76aad930db800c8d1565f470c9969b712f43b05a
Minna Ylitalo <minna@minnaliisi.com>
2018-07-16 18:09:01 +0100

    added comments in code

--- hangman/script.js
+++ hangman/script.js
@@ -2,7 +2,7 @@
 var url = "http://hangman-api.herokuapp.com/hangman";
 
 function processStartGame(data){
-  $("input.token").val(data.token);
+  $("input.token").val(data.token); //this is the game instance data 'token'
   $(".hangman-word").text(data.hangman);
   $("#new-game").hide();
 }
@@ -10,16 +10,16 @@ function processStartGame(data){
 function startGame(){
   $.ajax(url,
   {
-    method: "POST"
+    method: "POST" //sends a request for a new game
   }).done(processStartGame);
 }
 
 function makeGuess(){
-  var guess = $("input.letter").val();
+  var guess = $("input.letter").val(); //the value is what you have guessed
   var token = $(".token").val();
   $.ajax(url,
   {
-    method: "PUT",
+    method: "PUT", //sending guess to API
     data: {token: token, letter: guess}
   }).done(function (data) {
     processGuessResult(data, guess);
@@ -29,16 +29,17 @@ function makeGuess(){
 function processGuessResult(data, guess){
   $(".hangman-word").text(data.hangman);
   $("input.token").val(data.token);
-  var spanHtml = "<span class='";
+  var spanHtml = "<span class='"; //changes the CLASS of the html element
   if (data.correct) {
     spanHtml += "correct";
   } else {
     spanHtml += "wrong";
   }
-  spanHtml += "'>" + guess + "</span>";
+
+  spanHtml += "'>" + guess + "</span>"; //adds the guess to the page - class determines colour
   $(".attempts").append(spanHtml);
-  var wrongAttempts = $('.wrong').length;
-  $(".remaining").text(7 - wrongAttempts);
+  var wrongAttempts = $('.wrong').length; //reduces the amount of guesses
+  $(".remaining").text(7 - wrongAttempts); //.text changes the actual TEXT!
   var token = $(".token").val();
   if (wrongAttempts > 6) {
     $(".console").hide();
@@ -46,9 +47,9 @@ function processGuessResult(data, guess){
     $.ajax(url,
     {
       method: "GET",
-      data: {token: token}
+      data: {token: token} //retrieves solution when attempts have ran out
     }).done(function(data){
-      $(".hangman-word").text(data.solution);
+      $(".hangman-word").text(data.solution); //reveals the solution
     })
   }
 }
